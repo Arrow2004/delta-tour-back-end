d247219068d2f54115b73b6275df33dc5484bd77
Arrow2004 <arrow2004hakcer@gmail.com>
2023-03-11 16:22:48 +0500

    Test deploy

--- controllers/createPDF.js
+++ controllers/createPDF.js
@@ -8,22 +8,19 @@ module.exports = async (req,res)=>{
     console.log(req.body)
     newClient.save().then(client=>{
         const { _id, number, city, members, dateNow, dateFrom, dateTo, aviaTicket, transfer, hotel, foodType, medical, visas, cost, val, payed, passport, phoneNumber, firstName, lastName, fatherName} = client;
-        res.sendFile(path.join(__dirname,'../output','DELTA.png'),function (err){
-            if(err) res.json(err)
-        })
-        // generateCard({_id, number, city, members, aviaTicket, transfer, hotel, foodType, medical, visas, cost, val, payed, passport, phoneNumber, firstName, lastName, fatherName,dateNow: formatDate(dateNow), dateTo: formatDate(dateTo), dateFrom: formatDate(dateFrom)}).then(file =>{
-        //     // res.sendFile(path.join(file),{headers: {
-        //     //         '_id': _id
-        //     //     }},function (err){
-        //     //         fs.unlink(file, (err=>{
-        //     //             if(err) console.log(err);
-        //     //         }))
-        //     //         if(err) res.json({error: err, type: "sending"})
-        //     //     });
-        //     res.sendFile(path.join('../DELTA.png'),function (err){
-        //         if(err) res.json(err)
-        //     })
-        //     }).catch(err=> res.json({error: err, type: "creating"}));
+        generateCard({_id, number, city, members, aviaTicket, transfer, hotel, foodType, medical, visas, cost, val, payed, passport, phoneNumber, firstName, lastName, fatherName,dateNow: formatDate(dateNow), dateTo: formatDate(dateTo), dateFrom: formatDate(dateFrom)}).then(file =>{
+            res.sendFile(path.join(__dirname,'../output',_id+'.png'),{headers: {
+                    '_id': _id
+                }},function (err){
+                    fs.unlink(file, (err=>{
+                        if(err) console.log(err);
+                    }))
+                    if(err) res.json({error: err, type: "sending"})
+                });
+            res.sendFile(path.join('../DELTA.png'),function (err){
+                if(err) res.json(err)
+            })
+            }).catch(err=> res.json({error: err, type: "creating"}));
         }).catch(err=>{
             return res.json({error: err,  type: "saving"})
         })
