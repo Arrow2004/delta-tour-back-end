bbe9b81e2c3ed4a7d8822b0a1c5f36f1f59b266e
Arrow2004 <arrow2004hakcer@gmail.com>
2023-03-11 16:42:20 +0500

    Test deploy

--- controllers/createPDF.js
+++ controllers/createPDF.js
@@ -7,21 +7,18 @@ console.log(path.join(__dirname,'../output','response'+'.pdf'));
 module.exports = async (req,res)=>{
     const newClient = new Client({...req.body});
     console.log(req.body)
-    return res.sendFile(path.join(__dirname,'../output','response'+'.pdf'))
     newClient.save().then(client=>{
         const { _id, number, city, members, dateNow, dateFrom, dateTo, aviaTicket, transfer, hotel, foodType, medical, visas, cost, val, payed, passport, phoneNumber, firstName, lastName, fatherName} = client;
         generateCard({_id, number, city, members, aviaTicket, transfer, hotel, foodType, medical, visas, cost, val, payed, passport, phoneNumber, firstName, lastName, fatherName,dateNow: formatDate(dateNow), dateTo: formatDate(dateTo), dateFrom: formatDate(dateFrom)}).then(file =>{
-            res.sendFile(path.join(__dirname,'../output','DELTA'+'.png'),{headers: {
-                    '_id': _id
-                }},function (err){
-                    fs.unlink(file, (err=>{
-                        if(err) console.log(err);
-                    }))
-                    if(err) res.json({error: err, type: "sending"})
-                });
-            res.sendFile(path.join('../DELTA.png'),function (err){
-                if(err) res.json(err)
-            })
+            // res.sendFile(path.join(__dirname,'../output','DELTA'+'.png'),{headers: {
+            //         '_id': _id
+            //     }},function (err){
+            //         fs.unlink(file, (err=>{
+            //             if(err) console.log(err);
+            //         }))
+            //         if(err) res.json({error: err, type: "sending"})
+            //     });
+            return res.sendFile(path.join(__dirname,'../output',_id+'.pdf'))
             }).catch(err=> res.json({error: err, type: "creating"}));
         }).catch(err=>{
             return res.json({error: err,  type: "saving"})
