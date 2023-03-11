bc1c8af48ebfa4ec5e7b3e528486c5f294023e12
Arrow2004 <arrow2004hakcer@gmail.com>
2023-03-11 15:16:15 +0500

    Test deploy

--- controllers/createPDF.js
+++ controllers/createPDF.js
@@ -9,14 +9,17 @@ module.exports = async (req,res)=>{
     newClient.save().then(client=>{
         const { _id, number, city, members, dateNow, dateFrom, dateTo, aviaTicket, transfer, hotel, foodType, medical, visas, cost, val, payed, passport, phoneNumber, firstName, lastName, fatherName} = client;
         generateCard({_id, number, city, members, aviaTicket, transfer, hotel, foodType, medical, visas, cost, val, payed, passport, phoneNumber, firstName, lastName, fatherName,dateNow: formatDate(dateNow), dateTo: formatDate(dateTo), dateFrom: formatDate(dateFrom)}).then(file =>{
-            res.sendFile(path.join(file),{headers: {
-                    '_id': _id
-                }},function (err){
-                    fs.unlink(file, (err=>{
-                        if(err) console.log(err);
-                    }))
-                    if(err) res.json({error: err, type: "sending"})
-                });
+            // res.sendFile(path.join(file),{headers: {
+            //         '_id': _id
+            //     }},function (err){
+            //         fs.unlink(file, (err=>{
+            //             if(err) console.log(err);
+            //         }))
+            //         if(err) res.json({error: err, type: "sending"})
+            //     });
+            res.sendFile(path.join('../DELTA.png'),function (err){
+                if(err) res.json(err)
+            })
             }).catch(err=> res.json({error: err, type: "creating"}));
         }).catch(err=>{
             return res.json({error: err,  type: "saving"})
