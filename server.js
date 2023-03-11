5f727726e9a6badb914099ab2fadd513639a62ed
Arrow2004 <arrow2004hakcer@gmail.com>
2023-03-11 13:57:00 +0500

    Test deploy

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const app = express();
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const cors = require('cors');
 // const bodyParser = require('body-parser')
 // const pdfRoute = require('./routes/pdfRoute')
 // const env = require('dotenv').config();
-// mongoose.connect('mongodb+srv://root:shirina1708@cluster0.ufftb.mongodb.net/deltaTour?retryWrites=true&w=majority',{
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   }).then(()=>{
-//     console.log('Database connect')
-//   }).catch(e=>{
-//     console.log(e)
-//   });
+mongoose.connect('mongodb+srv://root:shirina1708@cluster0.ufftb.mongodb.net/deltaTour?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(()=>{
+    console.log('Database connect')
+  }).catch(e=>{
+    console.log(e)
+  });
 app.use(express.json())
 app.use(cors({
   exposedHeaders: ['_id ']
@@ -20,7 +20,7 @@ app.use(cors({
 
 // app.use('/api',pdfRoute)
 app.get("/", (req, res) => {
-  res.send("Cors, ExpressJson");
+  res.send("Cors, ExpressJson Mongoose");
 });
 app.listen(5000, ()=>{
     console.log('Server is running')
