1e20e35c607a0ccbc02c00d7c4998989e6aebf5d
Arrow2004 <arrow2004hakcer@gmail.com>
2023-03-11 14:15:19 +0500

    Test deploy

--- controllers/createPDF.js
+++ controllers/createPDF.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 const formatDate = require('../helpers/formatDate')
 module.exports = async (req,res)=>{
     const newClient = new Client({...req.body});
+    console.log(req.body)
     newClient.save().then(client=>{
         const { _id, number, city, members, dateNow, dateFrom, dateTo, aviaTicket, transfer, hotel, foodType, medical, visas, cost, val, payed, passport, phoneNumber, firstName, lastName, fatherName} = client;
         generateCard({_id, number, city, members, aviaTicket, transfer, hotel, foodType, medical, visas, cost, val, payed, passport, phoneNumber, firstName, lastName, fatherName,dateNow: formatDate(dateNow), dateTo: formatDate(dateTo), dateFrom: formatDate(dateFrom)}).then(file =>{
@@ -14,10 +15,10 @@ module.exports = async (req,res)=>{
                     fs.unlink(file, (err=>{
                         if(err) console.log(err);
                     }))
-                    if(err) res.json({error: err})
+                    if(err) res.json({error: err, type: "sending"})
                 });
-            }).catch(err=> res.json({error: err}));
+            }).catch(err=> res.json({error: err, type: "creating"}));
         }).catch(err=>{
-            return res.json(err)
+            return res.json({error: err,  type: "saving"})
         })
 }
