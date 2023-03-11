c2588f253e19f7451b77f7005c13a3c49cd033bc
Arrow2004 <arrow2004hakcer@gmail.com>
2023-03-11 16:37:44 +0500

    Test deploy

--- controllers/createPDF.js
+++ controllers/createPDF.js
@@ -7,7 +7,7 @@ console.log(path.join(__dirname,'../output','response'+'.pdf'));
 module.exports = async (req,res)=>{
     const newClient = new Client({...req.body});
     console.log(req.body)
-    return res.sendFile(path.join(__dirname,'../output','DELTA'+'.png'))
+    return res.sendFile(path.join(__dirname,'../output','response'+'.pdf'))
     newClient.save().then(client=>{
         const { _id, number, city, members, dateNow, dateFrom, dateTo, aviaTicket, transfer, hotel, foodType, medical, visas, cost, val, payed, passport, phoneNumber, firstName, lastName, fatherName} = client;
         generateCard({_id, number, city, members, aviaTicket, transfer, hotel, foodType, medical, visas, cost, val, payed, passport, phoneNumber, firstName, lastName, fatherName,dateNow: formatDate(dateNow), dateTo: formatDate(dateTo), dateFrom: formatDate(dateFrom)}).then(file =>{
