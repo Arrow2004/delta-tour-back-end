20cf5729ebdc2a8b10f58df6e300beb5634b55d3
Arrow2004 <arrow2004hakcer@gmail.com>
2023-03-12 13:57:35 +0500

    Deploy to Vercel  test 005 12.03

--- controllers/download.js
+++ controllers/download.js
@@ -8,12 +8,13 @@ module.exports = (req,res)=>{
     Client.findOne({_id}).then(client=>{        
         const {_id,number,city,members,dateNow,dateFrom,dateTo,aviaTicket,transfer,hotel,foodType,medical,visas,cost,val,payed,passport,phoneNumber,firstName,lastName,fatherName} = client;
             generateCard({_id, number, city, members, aviaTicket, transfer, hotel, foodType, medical, visas, cost, val, payed, passport, phoneNumber, firstName, lastName, fatherName,dateNow: formatDate(dateNow), dateTo: formatDate(dateTo), dateFrom: formatDate(dateFrom)}).then(file =>{
-                res.sendFile(path.join(file),function (err){
-                    fs.unlink(file, (err=>{
-                        if(err) console.log(err);
-                    }))
-                    if(err) res.json({error: err})
-                });
+                // res.sendFile(path.join(file),function (err){
+                //     fs.unlink(file, (err=>{
+                //         if(err) console.log(err);
+                //     }))
+                //     if(err) res.json({error: err})
+                // });
+                res.json({_id,file})
             }).catch(err=> res.json({error: err}));
         }).catch(err=>{
             res.json(err)
