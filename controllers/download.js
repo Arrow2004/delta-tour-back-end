6e7a524ddee68fe4d05b94a6fd958c18e05d0375
Arrow2004 <arrow2004hakcer@gmail.com>
2023-03-13 14:56:21 +0500

    Deploy to Vercel  test 23 13.03

--- controllers/download.js
+++ controllers/download.js
@@ -7,16 +7,6 @@ module.exports = async (req,res,next)=>{
     const client = await Client.findById(req.body._id).exec();
     const {_id,number,city,members,dateNow,dateFrom,dateTo,aviaTicket,transfer,hotel,foodType,medical,visas,cost,val,payed,passport,phoneNumber,firstName,lastName,fatherName} = client;
     const fileName = await generateCard({_id, number, city, members, aviaTicket, transfer, hotel, foodType, medical, visas, cost, val, payed, passport, phoneNumber, firstName, lastName, fatherName,dateNow: formatDate(dateNow), dateTo: formatDate(dateTo), dateFrom: formatDate(dateFrom)})
-    res.sendFile(path.resolve( process.cwd(),'output',`${_id}.pdf`),function (err){
-        if (err) {
-            next(err);
-          } else{
-            try {
-                fs.unlink(path.resolve( process.cwd(),'output',`${_id}.pdf`)); 
-              } catch(e) {
-                console.log("error removing ", path.resolve( process.cwd(),'output',`${_id}.pdf`)); 
-              }
-          }
-    });
+    res.sendFile(path.resolve( process.cwd(),'output',`${_id}.pdf`));
     
 }
