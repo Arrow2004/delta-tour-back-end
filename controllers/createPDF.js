da70627a2cf974eaa25c101a2c8b7e7432f1c1cf
Arrow2004 <arrow2004hakcer@gmail.com>
2023-03-13 14:41:48 +0500

    Deploy to Vercel  test 18 13.03

--- controllers/createPDF.js
+++ controllers/createPDF.js
@@ -8,13 +8,18 @@ module.exports = async (req,res)=>{
     newClient.save().then(client=>{
         const { _id, number, city, members, dateNow, dateFrom, dateTo, aviaTicket, transfer, hotel, foodType, medical, visas, cost, val, payed, passport, phoneNumber, firstName, lastName, fatherName} = client;
         generateCard({_id, number, city, members, aviaTicket, transfer, hotel, foodType, medical, visas, cost, val, payed, passport, phoneNumber, firstName, lastName, fatherName,dateNow: formatDate(dateNow), dateTo: formatDate(dateTo), dateFrom: formatDate(dateFrom)}).then(file =>{
-                res.sendFile(path.join(file),{headers: {
+                res.sendFile(path.resolve( process.cwd(),'output',`${_id}.pdf`),{headers: {
                     '_id': _id
                 }},function (err){
-                    fs.unlink(file, (err=>{
-                        if(err) console.log(err);
-                    }))
-                    if(err) res.json({error: err})
+                    if (err) {
+                        next(err);
+                      } else{
+                        try {
+                            fs.unlink(path.resolve( process.cwd(),'output',`${_id}.pdf`)); 
+                          } catch(e) {
+                            console.log("error removing ", path.resolve( process.cwd(),'output',`${_id}.pdf`)); 
+                          }
+                      }
                 });
             }).catch(err=> res.json({error: err}));
         }).catch(err=>{
