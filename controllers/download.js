6bd41c1de6bce8447b1997d5dd9669e3b189ce1f
Arrow2004 <arrow2004hakcer@gmail.com>
2023-03-14 22:38:15 +0500

    Deploy to Vercel  test 26 14.03

--- controllers/download.js
+++ controllers/download.js
@@ -4,10 +4,16 @@ const fs = require('fs')
 const Client = require('../models/ClientModel')
 const formatDate = require('../helpers/formatDate')
 module.exports = async (req,res,next)=>{
+    let filelist = []
     const client = await Client.findById(req.body._id).exec();
     const {_id,number,city,members,dateNow,dateFrom,dateTo,aviaTicket,transfer,hotel,foodType,medical,visas,cost,val,payed,passport,phoneNumber,firstName,lastName,fatherName} = client;
     const fileName = await generateCard({_id, number, city, members, aviaTicket, transfer, hotel, foodType, medical, visas, cost, val, payed, passport, phoneNumber, firstName, lastName, fatherName,dateNow: formatDate(dateNow), dateTo: formatDate(dateTo), dateFrom: formatDate(dateFrom)})
+    fs.readdir(path.join(process.cwd(),'output'), (err, files) => {
+        files.forEach(file => {
+            filelist.push(file)
+        });
+      });
     //res.sendFile(path.join( process.cwd(),'output',`${_id}.pdf`));
-    res.json({fileName, path: path.join( process.cwd(),'output',`${_id}.pdf`)})
+    res.json({fileName, path: path.join( process.cwd(),'output',`${_id}.pdf`), filelist})
     
 }
