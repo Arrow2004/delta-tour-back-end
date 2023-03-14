81ca6b04feb0c18c5c9295ccb40bbdf24a09738a
Arrow2004 <arrow2004hakcer@gmail.com>
2023-03-14 22:58:43 +0500

    Deploy to Vercel  test 29 14.03

--- pdfCreator/index.js
+++ pdfCreator/index.js
@@ -16,7 +16,7 @@ const generateCard = async (data)=>{
             ...data,
           },
         },
-       path: `./output/${data._id}.pdf`,
+       path: path.join(process.cwd(),'output',`${data._id}.pdf`),
        //path: `./output/1677488876677_john_doe_12345678.pdf`,
         type: "",
       };
